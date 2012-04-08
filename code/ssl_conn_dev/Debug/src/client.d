89ea3c6a245d1c17a4da98c51bcb2c6dc2951137
xcrkx <crk@crook.de>
2012-04-09 00:40:27 +0200

    clean up

--- code/ssl_conn_dev/Debug/src/client.d
+++
@@ -1,6 +0,0 @@
-src/client.d src/client.o: ../src/client.cc ../src/client.hh \
- ../src/ssl_conn.hh
-
-../src/client.hh:
-
-../src/ssl_conn.hh:
--- code/ssl_conn_dev/Debug/src/sim.d
+++
@@ -1,5 +0,0 @@
-src/sim.d src/sim.o: ../src/sim.cc ../src/client.hh ../src/server.hh
-
-../src/client.hh:
-
-../src/server.hh:
